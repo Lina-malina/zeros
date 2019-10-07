117b14d18ee67bd29927943081e50ab6728a3a66
Angelina Pobyvanets <gela4w@gmail.com>
2019-10-07 15:40:38 +0300

    feat: implement solution for zeros task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function zeros(expression) {
-  // your solution
+  let counter = 0;
+  let factorials = expression.split('*');
+  let areAllFactorialsOdd = true;
+
+  for (let i = 0; i < factorials.length; i++) {
+
+    // For double factorials
+    if (factorials[i].includes('!!')) {
+      let number = factorials[i].replace('!!', '');
+
+      // for double even factorials
+      if (number % 2 == 0) {
+        areAllFactorialsOdd = false;
+        for (let j = 10; number / j >= 1; j *= 5) {
+          counter += Math.floor(number / j);
+        }
+      }
+
+      // for double odd factorial
+      if (number % 2 != 0) {
+        counter += Math.round(number / 10) + Math.ceil((number - 24) / 50);
+      }
+
+      // For single factorial
+    } else if (factorials[i].includes('!')) {
+      areAllFactorialsOdd = false;
+      let number = factorials[i].replace('!', '');
+      for (let j = 5; number / j >= 1; j *= 5) {
+        counter += Math.floor(number / j);
+      }
+    }
+  }
+  return areAllFactorialsOdd == true ? 0 : counter;
 }
+
